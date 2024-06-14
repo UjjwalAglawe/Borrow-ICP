4ef6400082795fc04bcbcc949815d6b50543c895
UjjwalAglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2024-06-14 13:11:06 +0530

    Adding new info to main page

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
         <div className="gradient-bg-welcome">
           {/* <button onClick={connect}>Con</button> */}
 
-          <Nav account={account} />
+          <Nav account={account} loadProvider={loadProvider} loading={loading}/>
           {
             loading ? (<>
               {/* <div className=' text-white'>Connecting to Metamask</div> */}
